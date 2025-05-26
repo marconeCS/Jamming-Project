df998a2c1dbafb9c3aa41cfaeb264f0946a9777e
Madrilena <lazarmarcu@gmail.com>
2025-05-26 19:03:30 +0300

    add SearchBar

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+
+function App(){
+    return(
+        <SearchBar />
+    )
+}
+export default App;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SearchBar(){
+    return(
+        <>
+        <form value= '' action="" >
+            <input type="text"/>
+        </form>
+        </>
+    )
+}
+export default SearchBar;
